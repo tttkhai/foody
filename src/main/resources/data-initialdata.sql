30306c01f6b3fe8ee4803b5c2b458171835d99e1
Khai <khai_tran2@apple.com>
2020-03-23 10:38:28 -0400

    add jsonignore and data.sql

---
+++ src/main/resources/data-initialdata.sql
@@ -0,0 +1,18 @@
+-- insert country values
+-- INSERT INTO `foodydb`.`country` (`id`, `name`, `phone_code`) VALUES ('1', 'US', '+1');
+
+-- insert state values
+-- INSERT INTO `foodydb`.`state` (`id`, `abbreviation`, `name`, `country_id`) VALUES (1, 'GA', 'Georgia', 1);
+-- INSERT INTO `foodydb`.`state` (`id`, `abbreviation`, `name`, `country_id`) VALUES (2, 'TX', 'Texas', 1);
+-- INSERT INTO `foodydb`.`state` (`id`, `abbreviation`, `name`, `country_id`) VALUES (3, 'OH', 'Ohio', 1);
+-- INSERT INTO `foodydb`.`state` (`id`, `abbreviation`, `name`, `country_id`) VALUES (4, 'CA', 'California', 1);
+
+-- insert city values
+-- INSERT INTO `foodydb`.`city` (`id`, `name`, `zip_code`,`state_id`) VALUES (1, 'Riverdale', '30274', 1);
+-- INSERT INTO `foodydb`.`city` (`id`, `name`, `zip_code`,`state_id`) VALUES (2, 'Austin', '78758', 2);
+-- INSERT INTO `foodydb`.`city` (`id`, `name`, `zip_code`,`state_id`) VALUES (3, 'Milford', '45067', 3);
+-- INSERT INTO `foodydb`.`city` (`id`, `name`, `zip_code`,`state_id`) VALUES (4, 'Santa Clara', '95050', 4);
+-- INSERT INTO `foodydb`.`city` (`id`, `name`, `zip_code`,`state_id`) VALUES (5, 'Morrow', '30260', 1);
+
+
+
