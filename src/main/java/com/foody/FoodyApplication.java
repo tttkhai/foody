b92c45e104d380d70ae098bfddf2821c6866fe9b
Sau <sau@Khais-MacBook-Pro.local>
2020-04-11 17:06:26 -0400

    add cors config

--- src/main/java/com/foody/FoodyApplication.java
+++ src/main/java/com/foody/FoodyApplication.java
@@ -2,15 +2,30 @@ package com.foody;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @SpringBootApplication
+@Configuration
 //@EnableJpaRepositories(basePackages ={"com.foody.repository"})
 public class FoodyApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FoodyApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:8081");
+			}
+		};
 	}
 
 }
